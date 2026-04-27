f8559f9795a78722b1a5fe8af95ea0cdfe5483db
Yasanga32 <yasangashanuka33@gmail.com>
2026-04-27 17:28:37 +0530

    admin page add

---
+++ app/api/blog/route.js
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server"
+import ConnectDB from "../../../lib/config/db"
+import { writeFile } from "fs/promises";
+import { title } from "process";
+import BlogModel from "../../../lib/models/BlogModel";
+
+const LoadDB = async () => {
+    await ConnectDB();
+}
+
+LoadDB();
+
+
+
+export async function GET(request) {
+
+    return NextResponse.json({ msg: "API WORKING" })
+}
+
+export async function POST(request) {
+    const formData = await request.formData();
+    const timestamp = Date.now();
+
+    const image = formData.get('image');
+    const imageByteData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByteData);
+
+    const path = `./public/${timestamp}_${image.name}`;
+    await writeFile(path, buffer);
+
+    const imgUrl = `/${timestamp}_${image.name}`;
+
+
+
+    const blogData = {
+        title: `${formData.get('title')}`,
+        description: `${formData.get('description')}`,
+        category: `${formData.get('category')}`,
+        author: `${formData.get('author')}`,
+        image: `${imgUrl}`,
+        authorImg: `${formData.get('authorImg')}`
+    }
+
+    await BlogModel.create(blogData);
+    console.log("Blog created successfully", blogData);
+
+
+    return NextResponse.json({ success: true, msg: "Blog Added" })
+
+}
--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -2,6 +2,8 @@
 import { assets, blog_data } from '@/Assets/assets';
 import Image from 'next/image';
 import React, { useEffect, useState, use } from 'react'
+import Footer from '@/components/Footer';
+import Link from 'next/link';
 
 export default function Page({
   params,
@@ -24,20 +26,53 @@ export default function Page({
 
   return (
     data ? (
-      <div className="bg-gray-200 py-5 md:px-12 lg:px-28">
-        <div className="flex justify-between items-center">
-            <h1 className="text-3xl font-bold">Blog Details</h1>
-        </div>
-        <div className="text-center my-24">
+      <>
+        <div className="bg-gray-200 py-5 px-5 md:px-12 lg:px-28">
+          <div className="flex justify-between items-center">
+            <Link href='/'>
+              <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
+            </Link>
+            <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>
+              Get started <Image src={assets.arrow} alt='' />
+            </button>
+          </div>
+          <div className="text-center my-24">
             <h1 className="text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto">{data.title}</h1>
-            <Image className="mx-auto mt-6 border-4 border-white" src={data.image} width={800} height={450} alt="" />
-            <p className="mt-8 pb-9 text-lg max-w-[740px] mx-auto">{data.description}</p>
+            <Image className='mx-auto mt-6 border-4 border-white rounded-full' src={data.author_img} width={60} height={60} alt='' />
+            <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto'>{data.author}</p>
+          </div>
         </div>
-      </div>
+        <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
+          <Image className='border-4 border-white' src={data.image} width={1280} height={720} alt='' />
+          <h1 className='my-8 text-[26px] font-semibold'>Introduction</h1>
+          <p>{data.description}</p>
+          <h3 className='my-5 text-[18px] font-semibold'>Step 1: Self-Reflection and Goal Setting</h3>
+          <p className='my-3'>Before you can begin to manage your lifestyle, you must first understand what you want to achieve. Take the time to reflect on your current habits and identify areas for improvement.</p>
+          <p className='my-3'>Whether it is your health, career, or personal relationships, setting clear and achievable goals is the first step towards a more organized and fulfilling life.</p>
+          <h3 className='my-5 text-[18px] font-semibold'>Step 2: Prioritization and Time Management</h3>
+          <p className='my-3'>Once you have set your goals, it is time to prioritize them. Not all goals are created equal, and some will require more time and effort than others.</p>
+          <p className='my-3'>Learn to manage your time effectively by breaking down large tasks into smaller, more manageable ones. Use tools like calendars and to-do lists to keep yourself on track.</p>
+          <h3 className='my-5 text-[18px] font-semibold'>Step 3: Consistency and Adaptation</h3>
+          <p className='my-3'>The key to long-term lifestyle management is consistency. It is not about making massive changes overnight, but rather about making small, sustainable improvements every day.</p>
+          <p className='my-3'>Be prepared to adapt your plan as you go along. Life is unpredictable, and being flexible will help you stay on course even when things do not go as planned.</p>
+          <h3 className='my-5 text-[18px] font-semibold'>Conclusion:</h3>
+          <p className='my-3'>Managing your lifestyle is a continuous journey, not a destination. By following these steps and staying committed to your goals, you can create a life that is balanced, productive, and truly exceptional.</p>
+          
+          <div className='my-24'>
+            <p className='text-black font-semibold my-4'>Share this article on social media</p>
+            <div className='flex gap-2'>
+              <Image src={assets.facebook_icon} width={50} alt='' />
+              <Image src={assets.twitter_icon} width={50} alt='' />
+              <Image src={assets.googleplus_icon} width={50} alt='' />
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </>
     ) : (
       <div className="flex items-center justify-center min-h-screen">
         <h1 className="text-2xl">Loading...</h1>
       </div>
     )
   )
-}
+}
